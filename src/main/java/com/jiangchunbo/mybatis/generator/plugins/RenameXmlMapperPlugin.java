c2f2d70d930d9840bb872cb25ebdaf82033ebdd8
JiangChunbo <945086245@qq.com>
2023-03-14 00:45:21 +0800

    update

--- src/main/java/com/jiangchunbo/mybatis/generator/plugins/RenameXmlMapperPlugin.java
+++ src/main/java/com/jiangchunbo/mybatis/generator/plugins/RenameXmlMapperPlugin.java
@@ -12,33 +12,42 @@ import static org.mybatis.generator.internal.util.messages.Messages.getString;
 
 public class RenameXmlMapperPlugin extends PluginAdapter {
 
-    private final String searchString = "[A-Z][a-z]*";
-    private Pattern pattern;
-    
-    
-    public RenameXmlMapperPlugin() {
-        super();
-    }
+	private final String searchString = "[A-Z][a-z]*";
+	private Pattern pattern;
 
-    @Override
-    public boolean validate(List<String> warnings) {
-        pattern = Pattern.compile(searchString);
-        return true;
-    }
+	public RenameXmlMapperPlugin() {
+		super();
+	}
 
-    @Override
-    public void initialized(IntrospectedTable introspectedTable) {
-        String oldType = introspectedTable.getMyBatis3XmlMapperFileName();
-        Matcher matcher = pattern.matcher(oldType);
-        StringBuffer stringBuffer = new StringBuffer();
-        while(matcher.find()) {
-            if (matcher.start() == 0) {
-                matcher.appendReplacement(stringBuffer, matcher.group(0).toLowerCase());
-            } else {
-                matcher.appendReplacement(stringBuffer, "_" + matcher.group(0).toLowerCase());
-            }
-        }
-        stringBuffer.append("_mapping.xml");
-        introspectedTable.setMyBatis3XmlMapperFileName(stringBuffer.toString());
-    }
+	@Override
+	public boolean validate(List<String> warnings) {
+		pattern = Pattern.compile(searchString);
+		return true;
+	}
+
+	@Override
+	public void initialized(IntrospectedTable introspectedTable) {
+		String oldType = introspectedTable.getMyBatis3XmlMapperFileName();
+		if (oldType.endsWith("Mapper.xml")) {
+			oldType = oldType.substring(0, oldType.lastIndexOf("Mapper.xml"));
+		}
+		if (oldType.endsWith("DO")) {
+			oldType = oldType.substring(0, oldType.lastIndexOf("DO"));
+		}
+		Matcher matcher = pattern.matcher(oldType);
+		StringBuffer stringBuffer = new StringBuffer();
+		while (matcher.find()) {
+			if (matcher.start() == 0) {
+				matcher.appendReplacement(stringBuffer, matcher.group(0).toLowerCase());
+			} else {
+				matcher.appendReplacement(stringBuffer, "_" + matcher.group(0).toLowerCase());
+			}
+		}
+
+		introspectedTable.setMyBatis3JavaMapperType(introspectedTable.getMyBatis3JavaMapperType()
+				.substring(0, introspectedTable.getMyBatis3JavaMapperType().lastIndexOf(".") + 1).toString() + oldType
+				+ "Mapper");
+		stringBuffer.append("_mapping.xml");
+		introspectedTable.setMyBatis3XmlMapperFileName(stringBuffer.toString());
+	}
 }
